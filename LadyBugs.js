c15208812f502599da7b137eebd00449906abb9e
Iyadi Cyril <iyadicy@gmail.com>
2022-02-22 10:40:58 +0100

    Create LadyBugs.js
    HackerRank Lady Bug problem solution

---
+++ LadyBugs.js
@@ -0,0 +1,40 @@
+//https://www.hackerrank.com/challenges/happy-ladybugs/problem?isFullScreen=true
+
+function happyLadybugs(b) {
+    // Write your code here
+    let dict = {}
+    for(let i=0; i<b.length; i++){
+        if(dict[b[i]]){
+            dict[b[i]] += 1
+        } else {
+            dict[b[i]] = 1
+        }
+    }
+    let happy = true
+    let response = Object.keys(dict).map((key) => {
+        if(key != "_" && dict[key] == 1){
+            return 'NO'
+        }
+    })
+    console.log(response)
+    if(response.includes('NO')){
+        happy = false
+    }
+    
+    if(dict['_'] > 0 && happy == true){
+        happy = true
+    } else{
+        let pair = 0
+        for(let i=0; i<b.length-1; i++){ 
+            if(b[i] == b[i+1]){
+                pair++
+            } else if(pair > 0){
+                pair = 0
+            }else {
+                happy = false
+            }
+        }
+    }
+    
+    return happy ? "YES" : "NO"
+}
