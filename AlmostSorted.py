9d1c0b58937a5cd31bf868e18d7b35d0bb7c46f1
Iyadi Cyril <iyadicy@gmail.com>
2022-04-29 23:47:30 +0100

    Create AlmostSorted.py
    Hackerrank Almost Sorted challenge solution

---
+++ AlmostSorted.py
@@ -0,0 +1,37 @@
+# https://www.hackerrank.com/challenges/almost-sorted/problem?isFullScreen=true
+
+
+def almostSorted(arr):
+    # Write your code here
+    l = arr
+    sl = sorted(l)
+    diffcount = 0
+    diff1 = -1
+    diff2 = -1
+
+    for i in range(len(l)):
+        if sl[i] != l[i]:
+            diffcount += 1
+            if diff1 == -1:
+                diff1 = i
+            elif diffcount > 1:
+                diff2 = i
+            lastdiff = i
+
+    if diffcount == 2:
+        l[diff1], l[diff2] = l[diff2], l[diff1]
+        if l == sl:
+            print("yes")
+            print("swap {} {}".format(diff1 + 1, diff2 + 1))
+        else:
+            print("no")
+    elif diffcount > 2:
+        l = l[:diff1] + l[diff1:diff2 + 1][::-1] + l[diff2 + 1:]
+        if l == sl:
+            print("yes")
+            print("reverse {} {}".format(diff1 + 1, diff2 + 1))
+        else:
+            print("no")
+
+    elif l == sl:
+        print("yes")
