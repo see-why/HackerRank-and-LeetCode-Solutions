83cef1d79a14a352dd0e9c5cf5b46161d6f7aa39
Iyadi Cyril <iyadicy@gmail.com>
2022-05-14 13:52:39 +0100

    Update GridlandMetro.py
    Correct algorithm

--- GridlandMetro.py
+++ GridlandMetro.py
@@ -3,19 +3,30 @@
 def gridlandMetro(n, m, k, track):
     # Write your code here
     spots = n*m
+    track.sort(key = lambda track: min(track[1], track[2]))
+    total_track_spots = 0
     rows = dict()
 
-    for row in track: 
-        r = row[0]
-        c1 = row[1]
-        c2 = row[2]
-        if r not in rows:
-            rows[r] = (c1, c2)
-        else:
-            rows[r] = (min(c1, rows[r][0]), max(c2, rows[r][1]))
-
-    for (c1, c2) in rows.values():
-        length = (c2 - c1) + 1
-        spots -= length
+    for row, start_index, end_index in track:  
+        print(row, start_index, end_index)
+        if  start_index > end_index:
+           (start_index, end_index) = (end_index, start_index) 
+        
+        if row not in rows:                
+            total_track_spots += end_index - start_index +1 
+            rows[row] = end_index           
+            print(total_track_spots)         
+        else:            
+            if start_index > rows[row]:
+                total_track_spots += end_index - start_index + 1
+                rows[row] = end_index
+                print(total_track_spots) 
+            elif end_index > rows[row]:
+                total_track_spots += end_index - rows[row]
+                rows[row] = end_index
+                
+            print(rows[row]) 
+   
+    spots -= total_track_spots
 
     return spots
