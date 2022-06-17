9a14fc6148c1a368082d98276573ca3825991e9d
Iyadi Cyril <iyadicy@gmail.com>
2022-06-17 23:00:37 +0100

    Update MergeSort:CountingInversions.py
    Optimized solution

--- MergeSort:CountingInversions.py
+++ MergeSort:CountingInversions.py
@@ -1,62 +1,46 @@
 # https://www.hackerrank.com/challenges/ctci-merge-sort/problem?isFullScreen=true
 
-
-def _mergeSort(arr, temp_arr, left, right):
- 
-    inv_count = 0
- 
-    if left < right:
- 
-        mid = (left + right)//2
- 
-        inv_count += _mergeSort(arr, temp_arr,
-                                left, mid)
- 
-        inv_count += _mergeSort(arr, temp_arr,
-                                mid + 1, right)
- 
-        inv_count += merge(arr, temp_arr, left, mid, right)
-    return inv_count
-
- 
-def merge(arr, temp_arr, left, mid, right):
-    i = left     # Starting index of left subarray
-    j = mid + 1  # Starting index of right subarray
-    k = left     # Starting index of to be sorted subarray
-    inv_count = 0
- 
-    while i <= mid and j <= right:
- 
-        if arr[i] <= arr[j]:
-            temp_arr[k] = arr[i]
-            k += 1
-            i += 1
-        else:
-            # Inversion will occur.
-            temp_arr[k] = arr[j]
-            inv_count += (mid-i + 1)
-            k += 1
+def _mergeSort(a, l, m, h): 
+    c = []
+    i = l
+    j = m + 1
+    s = 0
+    
+    while i <= m and j <= h:
+        if a[i] > a[j]:
+            # there is an inversion
+            s += (m - i + 1)
+            c.append(a[j])
             j += 1
-
-    while i <= mid:
-        temp_arr[k] = arr[i]
-        k += 1
+        else:
+            c.append(a[i])
+            i += 1
+            
+    # Adding remaning numbers
+    while i <= m:
+        c.append(a[i])
         i += 1
-
-    while j <= right:
-        temp_arr[k] = arr[j]
-        k += 1
+    while j <= h:
+        c.append(a[j])
         j += 1
- 
-    # Copy the sorted subarray into Original array
-    for loop_var in range(left, right + 1):
-        arr[loop_var] = temp_arr[loop_var]
- 
-    return inv_count
+        
+    
+    a[l: h + 1] = c
+    
+    return s
+            
 
+def count(a, l, h):
+    if l >= h:
+        return 0
+    #print(l, h)
+    m = l + (h - l) // 2
+    s = 0
+    s += count(a, l, m)
+    s += count(a, m + 1, h)
+    
+    s += _mergeSort(a, l, m, h)
+    return s
 
-def countInversions(arr):
-    # Write your code here
-    n = len(arr)
-    temp_arr = [0]*n
-    return _mergeSort(arr, temp_arr, 0, n-1)
+def countInversions(a):
+    return count(a, 0, len(a) - 1)
