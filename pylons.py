bad4398c39277fd712ddde0cb75ac04aad0e6777
Iyadi Cyril <iyadicy@gmail.com>
2022-07-27 23:43:10 +0100

    Update pylons.py
    optimized solution

--- pylons.py
+++ pylons.py
@@ -1,19 +1,47 @@
 # https://www.hackerrank.com/challenges/pylons/problem?isFullScreen=true
 
-def voisins(i):
-    return range(min(n-1, i+k-1), max(-1, i-k), -1)
+def get_min(x,k,lights):        
+    best = -1
+    best_ind=None
+    for ind,i in enumerate(lights):
+        #if abs(x - i)<k:
+        if -k < x - i < k:
+            best=i
+            best_ind=ind
+        elif i>x+k:
+            break
+    return best,best_ind
 
 def pylons(k, arr):
     # Write your code here
-    count = 0
-    past_fourni = 0
-    while past_fourni < n:
-        for i in voisins(past_fourni):
-            if arr[i]:
-                past_fourni = i + k
-                count += 1
-                break
-            else:
-                count = -1
-                break                
+    lights = [i for i,val in enumerate(arr) if val==1]
+    
+    pos=0
+    best=None
+    best_ind=None
+    count=0
+ 
+    while 1:                                
+            
+        if pos>=n:
+            break
+        
+        if best==lights[-1]:
+            count=-1
+            break
+                       
+        if best_ind==None:
+            best,best_ind = get_min(pos,k,lights)
+        else:
+            lights = lights[(best_ind+1):]
+            best,best_ind = get_min(pos,k,lights)
+        
+        if best==-1:
+            count=-1
+            break
+        
+        count+=1
+        pos = best + k                                            
+
+    
     return(count)
