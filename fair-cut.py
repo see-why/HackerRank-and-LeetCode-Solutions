cc9b6e3b8ddfca5e1561a5aada81abde2c052a65
Iyadi Cyril <iyadicy@gmail.com>
2022-08-01 23:49:49 +0100

    Create fair-cut.py
    Hackerrank fair-cut challenge solution

---
+++ fair-cut.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/fair-cut/problem?isFullScreen=true
+
+
+def fairCut(k, arr):
+    # Write your code here
+    if 2*k > n:
+        k = n - k
+
+    a = sorted(arr)
+
+    # create dp
+
+    dp = [[float('inf') for i in range(0, n + 1)] for j in range(0, n + 1)]
+
+    dp[0][0] = 0
+
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            if i > k and i-j > n-k:
+                continue
+            to_li = dp[i][j] + a[i] * (i - j - (n - k - (i - j)))
+            to_lu = dp[i][j] + a[i] * (j - (k - j))
+            # to Li
+            if dp[i+1][j+1] > to_li:
+                dp[i+1][j+1] = to_li
+            
+            # to Lu
+            if dp[i+1][j] > to_lu:
+                dp[i+1][j] = to_lu
+
+    return(dp[n][k])
