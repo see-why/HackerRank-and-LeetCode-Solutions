8abd32ef716cbfa66cdfdfed7a5082e7650d8c06
Iyadi Cyril <iyadicy@gmail.com>
2022-08-05 23:54:05 +0100

    Create s10-basic-statistics.py
    Hackerrank declarative solution to Day 0: Mean, Median, and Mode

---
+++ s10-basic-statistics.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/s10-basic-statistics/problem?isFullScreen=true
+
+import numpy as np
+from scipy import stats
+
+size = int(input())
+numbers = list(map(int, input().split()))
+print(np.mean(numbers))
+print(np.median(numbers))
+print(int(stats.mode(numbers)[0]))
