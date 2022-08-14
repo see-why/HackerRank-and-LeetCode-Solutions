c0ffaa5185c389ac2ad340126c26d87510c6dcf4
Iyadi Cyril <iyadicy@gmail.com>
2022-08-14 23:47:14 +0100

    Create s10-binomial-distribution-1.py
    Hackerrank Day 4: Binomial Distribution I challenge solution

---
+++ s10-binomial-distribution-1.py
@@ -0,0 +1,14 @@
+#  https://www.hackerrank.com/challenges/s10-binomial-distribution-1/problem?isFullScreen=true
+
+def fact(n):
+    return 1 if n == 0 else n*fact(n-1)
+
+def comb(n, x):
+    return fact(n) / (fact(x) * fact(n-x))
+
+def b(x, n, p):
+    return comb(n, x) * p**x * (1-p)**(n-x)
+
+l, r = list(map(float, input().split(" ")))
+odds = l / r
+print(round(sum([b(i, 6, odds / (1 + odds)) for i in range(3, 7)]), 3))
