02a0060b9e0fc9cfba667a834fe54f8b187a49a4
Iyadi Cyril <iyadicy@gmail.com>
2022-09-23 23:43:28 +0100

    Create find_averages_of_subarrays.py
    finding averages of subarrays using sliding window pattern

---
+++ find_averages_of_subarrays.py
@@ -0,0 +1,22 @@
+# sliding window pattern
+
+def find_averages_of_subarrays(K, arr):
+  result = []
+  windowSum, windowStart = 0.0, 0
+  for windowEnd in range(len(arr)):
+    windowSum += arr[windowEnd]  # add the next element
+    # slide the window, we don't need to slide if we've not hit the required window size of 'k'
+    if windowEnd >= K - 1:
+      result.append(windowSum / K)  # calculate the average
+      windowSum -= arr[windowStart]  # subtract the element going out
+      windowStart += 1  # slide the window ahead
+
+  return result
+
+
+def main():
+  result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+  print("Averages of subarrays of size K: " + str(result))
+
+
+main()
