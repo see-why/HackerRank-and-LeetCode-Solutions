27bdb300ff4ec44c197aa1b639346486cf8bc99d
Iyadi Cyril <iyadicy@gmail.com>
2022-09-26 23:47:37 +0100

    Update longest_substring_with_k_distinct_characters.rb

--- longest_substring_with_k_distinct_characters.rb
+++ longest_substring_with_k_distinct_characters.rb
@@ -1,7 +1,29 @@
 
+# longest substring with K distinct characters
 
-Output
+def distinct_substring string, k
+    windows_start, counter, size = 0, 0, string.size-1
+    substring = []
+   
+    for windows_start in (0..size)
+        for windows_end in (windows_start..size)
+            substring << string[windows_end]
+           
+            if no_of_distinct_characters(substring) == k
+                counter = substring.size > counter ? substring.size : counter
+            end
+        end
+        substring.clear
+    end
+   
+    counter
+end
+           
+def no_of_distinct_characters string
+    string.to_a.uniq.size
+end
 
-Input: String="araaci", K=2
-Output: 4
-Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+p distinct_substring("araaci", 2)
+p distinct_substring("araaci", 1)
+p distinct_substring("cbbebi", 3)
