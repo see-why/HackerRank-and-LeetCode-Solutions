e13c2667a91749b35346b436dbfd55a5ddf41dca
Iyadi Cyril <iyadicy@gmail.com>
2022-09-28 10:30:41 +0100

    Update longest_substring_with_k_distinct_characters.rb
    Add sliding pattern algorithm

--- longest_substring_with_k_distinct_characters.rb
+++ longest_substring_with_k_distinct_characters.rb
@@ -27,3 +27,31 @@ end
 p distinct_substring("araaci", 2)
 p distinct_substring("araaci", 1)
 p distinct_substring("cbbebi", 3)
+
+def distinct_substring_sliding string, k
+    windows_start, max_length, size = 0, 0, string.size-1
+    char_hash = {}
+   
+    for windows_end in (0..size)
+        right_char = string[windows_end]
+        char_hash[right_char] = char_hash[right_char] ? char_hash[right_char] += 1 : 1
+       
+        while char_hash.size > k
+            left_char = string[windows_start]
+            char_hash[left_char] -= 1
+           
+            if char_hash[left_char] == 0
+                char_hash.delete(left_char)
+            end
+           
+            windows_start += 1
+            max_length = [max_length, windows_end-windows_start+1].max
+        end
+    end
+   
+    max_length
+end
+
+p distinct_substring_sliding("araaci", 2)
+p distinct_substring_sliding("araaci", 1)
+p distinct_substring_sliding("cbbebi", 3)
