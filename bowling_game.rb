5dff9c5651a0778860e75bf378fe40a8df996210
Iyadi Cyril <iyadicy@gmail.com>
2022-11-01 07:24:00 +0100

    Update bowling_game.rb
    refactored code and added links to scoring rules.

--- bowling_game.rb
+++ bowling_game.rb
@@ -15,27 +15,45 @@ class Playground
     'boring'
   end
 
-  def bowling_game(array, n = array.size - 1, first = array[0])
+  def bowling_game_recursive(array)
+    array, first, n = array_defaults(array)
     return first if n.zero?
-    return first + sum(array[1, 2]) + bowling_game(array[1, n]) if strike?(array)
+    return bonus_for_strike(array, n) if strike?(array)
+    return bonus_for_spare(array, n) if spare?(array)
 
-    return sum(array[0, 3]) + bowling_game(array[2, n]) if spare?(array)
+    standard_bonus(array, n)
+  end
 
-    first + bowling_game(array[1, n])
+  def bonus_for_strike(array, end_index)
+    TEN + sum(array[1, 2]) + bowling_game_recursive(array[1, end_index])
+  end
+
+  def bonus_for_spare(array, end_index)
+    TEN + array[2] + bowling_game_recursive(array[2, end_index])
+  end
+
+  def standard_bonus(array, end_index)
+    sum(array[0, 2]) + bowling_game_recursive(array[2, end_index])
   end
 
   def strike?(array)
     array[0] == TEN && array.size > 3
   end
 
+  def array_defaults(array)
+    [array ||= [0], array.first, (array.size - 1)]
+  end
+
   def spare?(array)
+    array[0] ||= 0
+    array[1] ||= 0
     array[0] + array[1] == TEN
   end
 
   def sum(array)
     array.inject(:+)
   end
-
 end
 
 # expect bowling_game([10] * 12) to be 300
+# bolwing games rules: https://www.rookieroad.com/bowling/scoring-rules/
