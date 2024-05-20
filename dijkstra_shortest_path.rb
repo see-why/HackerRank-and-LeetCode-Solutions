4a956d6158bed3d1a38f9fd8c9ad76d9246ddae6
Iyadi Cyril <iyadicy@gmail.com>
2024-05-20 10:59:36 +0100

    Create dijkstra_shortest_path.rb

---
+++ dijkstra_shortest_path.rb
@@ -0,0 +1,37 @@
+graph = {
+  'A' => { 'B' => 1, 'C' => 4 },
+  'B' => { 'A' => 1, 'C' => 2, 'D' => 5 },
+  'C' => { 'A' => 4, 'B' => 2, 'D' => 1 },
+  'D' => { 'B' => 5, 'C' => 1 }
+}
+
+def dijkstra (graph, start)
+  distances = {}
+  visited = {}
+
+  nodes = graph.keys
+  nodes.each { |node| distances[node] = Float::INFINITY }
+
+  distances[start] = 0
+
+  until nodes.empty?
+    min_node = nodes.min_by { |node| visited[node] ? Float::INFINITY : distances[node] }
+    puts "min_node: #{min_node}"
+    break if distances[min_node] == Float::INFINITY
+
+    graph[min_node].each do |neighbour, distance|
+      alt = distances[min_node] + distance
+      distances[neighbour] = alt if alt < distances[neighbour]
+    end
+
+    visited[min_node] = true
+    nodes.delete(min_node)
+  end
+
+  distances
+end
+
+puts dijkstra(graph, 'A')
+puts dijkstra(graph, 'B')
+puts dijkstra(graph, 'C')
+puts dijkstra(graph, 'D')
